5f8af42edccb5760a86d2920f7b3661d8e3f239f
Sooraj Gupta <soorajgupta00@gmail.com>
2024-03-28 14:05:47 +0530

    typo

--- index.js
+++ index.js
@@ -238,8 +238,7 @@ ${chalk.bold.underline.white("Package Commands:")}
   )}
       - But if you want to add node_modules folder with the template you are creating then ${chalk.cyan(
         "-y"
-      )} 
-      flag after the command.
+      )} flag after the command.
       ${chalk.yellow("Example:")} ${chalk.cyan("init -a")} ${chalk.yellow(
     '"C:\\Users\\{User}\\Desktop\\Projects\\Ongoing Projects"'
   )} ${chalk.cyan("-y")}
